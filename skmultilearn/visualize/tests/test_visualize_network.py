32c55556600b8e43946179875e2594e9b903a793
SharmaRupali <rups1501@gmail.com>
2017-11-14 00:39:38 +0100

    Visualization module network tests update - to be modified

--- skmultilearn/visualize/tests/test_visualize_network.py
+++ skmultilearn/visualize/tests/test_visualize_network.py
@@ -2,6 +2,7 @@ from sklearn.datasets import make_multilabel_classification
 import matplotlib.pyplot as plt
 import scipy.sparse as sp
 import unittest
+import types
 
 
 class VisualizeNetworkTests(unittest.TestCase):
@@ -12,4 +13,33 @@ class VisualizeNetworkTests(unittest.TestCase):
                                               allow_unlabeled=False)
 
         assert sp.issparse(y)
-        self.assertTrue()
+
+        VisualizeNetwork.visualize_input_network(x, y, labels=None, k=2, iterations=50, n_size=25,
+                                                 n_e_ratiotype="linear", linear_ratio_val=0.5, log_ratio_base_val=1.2,
+                                                 cmap_node="copper", cmap_edge=plt.cm.BrBG)
+
+        labels = self.labels
+
+
+        def test_works(self):
+            print('labels ='), self.labels
+            isinstance(labels, types.StringTypes)
+
+        parameters = {
+            'labels': None,
+            'k': 2,
+            'iterations': 50,
+            'n_size': 25,
+            'n_e_ratiotype': "linear",
+            'linear_ratio_val': 0.5,
+            'log_ratio_base_val': 1.2,
+            'cmap_node': "copper",
+            'cmap_edge': plt.cm.BrBG,
+        }
+
+        for p in list(parameters.keys()):
+            self.assertTrue(p, )
+
+
+if __name__ == '__main__':
+    unittest.main()
